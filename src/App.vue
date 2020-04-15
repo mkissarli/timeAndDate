ecd8c390183256986d11b0d636c222d0b3daf754
Malik <kissarlim@gmail.com>
2020-04-15 15:32:27 +0100

    Added area picker.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+  <timeDate></timeDate>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TimeDate from './components/TimeDate.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TimeDate
   }
 }
 </script>
---
+++ src/components/TimeDate.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="timeDateDash">
+  <label> Which area would you like to pick? </label>
+  <select id="areaPicker">
+    <option v-for="a in area" v-bind:key="a" v-bind:value="a"> {{ a }} </option>
+  </select>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'TimeDate',
+  props: {
+  },
+  data: () =>({
+    area: ["Africa", "America", "Asia", "Atlantic", "Australia", "Europe", "Indian", "Pacific"],
+  })
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
